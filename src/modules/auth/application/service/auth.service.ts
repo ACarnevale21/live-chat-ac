0fa2a559c34931482febe425e0f5a758af1d44ba
AgustinCarnvale <carnevaleagustinj@gmail.com>
2024-10-18 20:06:31 -0300

    refactor signIn function

--- src/modules/auth/application/service/auth.service.ts
+++ src/modules/auth/application/service/auth.service.ts
@@ -16,9 +16,12 @@ export class AuthService {
     pass: string,
   ): Promise<{ access_token: string }> {
     const user = await this.userService.findOne(username);
-    if (bcrypt.compare(pass, user?.password)) {
+
+    const isMatch = await bcrypt.compare(pass, user?.password);
+    if (!isMatch) {
       throw new UnauthorizedException();
     }
+
     const payload = { sub: user.id, username: user.username };
     return {
       access_token: await this.jwtService.signAsync(payload),
